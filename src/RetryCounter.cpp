7beec380077eb281e23ed526c19b62f2f4715281
k-rol <carol.ouellet@gmail.com>
2014-03-28 23:20:22 -0400

    added retrycounter class file

---
+++ src/RetryCounter.cpp
@@ -0,0 +1,18 @@
+/*
+ * RetryCounter.cpp
+ *
+ *  Created on: 2014-03-28
+ *      Author: CaroL
+ */
+
+#include "RetryCounter.h"
+
+RetryCounter::RetryCounter() {
+	// TODO Auto-generated constructor stub
+
+}
+
+RetryCounter::~RetryCounter() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/RetryCounter.h
@@ -0,0 +1,20 @@
+/*
+ * RetryCounter.h
+ *
+ *  Created on: 2014-03-28
+ *      Author: CaroL
+ */
+
+#ifndef RETRYCOUNTER_H_
+#define RETRYCOUNTER_H_
+
+/*
+ *
+ */
+class RetryCounter {
+public:
+	RetryCounter();
+	virtual ~RetryCounter();
+};
+
+#endif /* RETRYCOUNTER_H_ */
